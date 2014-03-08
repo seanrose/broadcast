e0cd882b12debfdd60e245ed9b34d5b16685f439
seanrose <seanrose@alumni.stanford.edu>
2014-03-08 14:12:07 -0800

    block reactivity in iframe with {{#constant}} block

--- client/views/presentations/presentation.js
+++ client/views/presentations/presentation.js
@@ -3,17 +3,23 @@ Template.presentation.helpers({
 });
 
 Template.presentation.rendered = function() {
-    viewer = Crocodoc.createViewer('.viewer', {
-        url: 'https://view-api.box.com/1/sessions/'+ this.data.sessionId + '/assets',
-        layout: Crocodoc.LAYOUT_PRESENTATION
-    });
-    viewer.load();
+    // Stupid hack that ensures this is only run on first rendering
+    if (!this.rendered) {
+        viewer = Crocodoc.createViewer('.viewer', {
+            url: 'https://view-api.box.com/1/sessions/'+ this.data.sessionId + '/assets',
+            layout: Crocodoc.LAYOUT_PRESENTATION
+        });
+        viewer.load();
+
+        // Set to true to ensure this block isn't run again
+        this.rendered = true;
+    }
 };
 
 Template.presentation.events({
-    'click button': function(e) {
+    'click button': function(event) {
         if (typeof viewer !== 'undefined') {
-            var $button = $(e.target);
+            var $button = $(event.target);
 
             if ($button.get(0).id === 'next') {
                 viewer.scrollTo(Crocodoc.SCROLL_NEXT);
