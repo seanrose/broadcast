04a65faa5eeca8c8cda55f2373f0c7d9a2d53c82
seanrose <seanrose@alumni.stanford.edu>
2014-03-09 00:22:24 -0800

    basic page syncing with Deps.autorun!

--- client/views/presentations/presentation.js
+++ client/views/presentations/presentation.js
@@ -3,31 +3,47 @@ Template.presentation.helpers({
 });
 
 Template.presentation.rendered = function() {
+    presentationData = this.data;
+
     // Stupid hack that ensures this is only run on first rendering
     if (!this.rendered) {
         viewer = Crocodoc.createViewer('.viewer', {
-            url: 'https://view-api.box.com/1/sessions/'+ this.data.sessionId + '/assets',
+            url: 'https://view-api.box.com/1/sessions/'+ presentationData.sessionId + '/assets',
             layout: Crocodoc.LAYOUT_PRESENTATION
         });
         viewer.load();
 
-        // Ghetto means of passing the new page to where we can log to the DB
+        // Update the DB whenever the page changes
         viewer.on('pagefocus', function(event) {
-            Session.set('page', event.data.page);
-            $('.page-tracker').trigger('click');
+            Presentations.update({_id: presentationData._id}, {
+                $set: {page: event.data.page}
+            });
         });
 
         // Set to true to ensure this block isn't run again
         this.rendered = true;
     }
+
+    Deps.autorun(function() {
+        var presentationCursor = Presentations.find({_id: presentationData._id});
+        currentPage = presentationCursor.fetch()[0].page;
+        // Must use try/catch block because of bug in Deps.afterFlush
+        try {
+            viewer.scrollTo(currentPage);
+        } catch (e) {
+            if (e.message !== "Cannot call method 'scrollTo' of undefined") {
+                throw(e);
+            }
+        }
+    });
 };
 
 Template.presentation.events({
-    'click button': function(event) {
-        event.preventDefault();
+    'click button': function(e) {
+        e.preventDefault();
 
         if (typeof viewer !== 'undefined') {
-            var $button = $(event.target);
+            var $button = $(e.target);
 
             if ($button.get(0).id === 'next') {
                 viewer.scrollTo(Crocodoc.SCROLL_NEXT);
@@ -35,11 +51,13 @@ Template.presentation.events({
                 viewer.scrollTo(Crocodoc.SCROLL_PREVIOUS);
             }
         }
+
+        return false;
     },
 
-    'click .page-tracker': function(event) {
-        Presentations.update({_id: this._id}, {
-            $set: {page: Session.get('page')}
-        });
+    'click .page-tracker': function(e) {
+        e.preventDefault();
+
+
     }
 });
