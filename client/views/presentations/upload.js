f4a896d7f473bc78d006d7ab828767cd0f2af67e
seanrose <seanrose@alumni.stanford.edu>
2014-04-03 17:50:53 -0700

    moved api key to Meteor.settings

--- client/views/presentations/upload.js
+++ client/views/presentations/upload.js
@@ -1,12 +1,12 @@
-Template.upload.rendered = function() {
-};
-
 Template.upload.events({
     'click #upload': function(e) {
         e.preventDefault();
         filepicker.setKey('A3UuvGmumRN273ixbsJnVz');
-        filepicker.pick(function(InkBlob){
-            Meteor.call('createPresentation', InkBlob.url);
+        filepicker.pick(function(InkBlob) {
+            Meteor.call('createPresentation', InkBlob.url, function(error, presentationId) {
+            	presentationLink = Router.routes['presenter'].path({_id: presentationId});
+            	$('#presentation-btn').attr('href', presentationLink).fadeIn('slow');
+            });
         });
     }
 });
--- server/api.js
+++ server/api.js
@@ -1,39 +1,29 @@
-BOX_VIEW_TOKEN = 'r6ql08a8nzsh9n0c7ez86nram80ze1sp'
-DOCUMENTS_URL = 'https://view-api.box.com/1/documents'
-SESSIONS_URL = 'https://view-api.box.com/1/sessions'
+BOX_VIEW_API_KEY = Meteor.settings.BOX_VIEW_API_KEY;
+DOCUMENTS_URL = 'https://view-api.box.com/1/documents';
+SESSIONS_URL = 'https://view-api.box.com/1/sessions';
 
 Meteor.methods({
 	createPresentation: function(fileUrl) {
-    	// First upload the document
+        // First upload the document
     	var options = {};
     	options.headers = {
-    		Authorization: 'Token ' + BOX_VIEW_TOKEN,
+    		Authorization: 'Token ' + BOX_VIEW_API_KEY,
     	};
     	options.data = {
     		url: fileUrl
     	};
 
     	var documentResponse = HTTP.call('POST', DOCUMENTS_URL, options);
-    	console.log(documentResponse);
-        // Now create a session
-        options.data = {
-        	document_id: documentResponse.data.id
-        }
 
-		var sessionResponse = {
-			data: {
-				statusCode: 202
-			}
-		};
+        // TODO(seanrose): handle failure case somehow lol
 
-		while (sessionResponse.data.statusCode !== 201) {
-			Meteor.setTimeout(function() {
-				sessionResponse = HTTP.call('POST', SESSIONS_URL, options);
-				console.log(sessionResponse);
-			}, 2000);
-		}
+        // Create the presentation in the DB
+        var presentation = Presentations.insert({
+            documentId: documentResponse.data.id,
+            sessionId: '',
+            page: 1
+        });
 
-        // Finally return the new session ID
-
+        return presentation;
     }
 });
