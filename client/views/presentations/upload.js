79ccc15102e52b171a00d61c5c5c15fc387cf072
seanrose <seanrose@alumni.stanford.edu>
2014-04-03 23:46:44 -0700

    redirects directly to presentation page now

--- client/views/presentations/upload.js
+++ client/views/presentations/upload.js
@@ -4,8 +4,7 @@ Template.upload.events({
         filepicker.setKey('A3UuvGmumRN273ixbsJnVz');
         filepicker.pick(function(InkBlob) {
             Meteor.call('createPresentation', InkBlob.url, function(error, presentationId) {
-            	presentationLink = Router.routes['presenter'].path({_id: presentationId});
-            	$('#presentation-btn').attr('href', presentationLink).fadeIn('slow');
+            	Router.go('presenter', {_id: presentationId});
             });
         });
     }
