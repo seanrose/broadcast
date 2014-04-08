e5df75167e99afd69d1d8108e82bac957ae09f3e
seanrose <seanrose@alumni.stanford.edu>
2014-04-07 18:34:51 -0700

    removed string hash package

--- packages/string-hash/string-hash_tests.js
+++
@@ -1,1 +0,0 @@
-// See https://github.com/dandv/meteor-crypto-base64/blob/master/crypto-base64_tests.js for a simple example// See https://www.eventedmind.com/feed/e6gJZXNQWyNP2MLsb for more on testing with Tinytest
