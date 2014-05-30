1e5432363fcc186ca15212828f76e97d0da0684c
seanrose <seanrose@alumni.stanford.edu>
2014-05-30 12:07:47 -0700

    refactored permissions for #9, i think ¯\_(ツ)_/¯

--- collections/presentations.js
+++ collections/presentations.js
@@ -2,13 +2,15 @@ Presentations = new Meteor.Collection('presentations');
 
 Presentations.allow({
 	update: function(userId, doc, fieldNames, modifier) {
+		var operations = _.keys(modifier);
 		return (
 			// Must have both page and presenterId field
-			!_.without(fieldNames, 'page', 'presenterId').length > 0
+			// !_.without(fieldNames, 'page', 'presenterId').length > 0
+			_.intersection(fieldNames, ['page', 'presenterId']).length === 2 &&
 			// Only allow $set
-			&& _.has(modifier, '$set')
+			_.intersection(operations, ['$set']).length === 1 &&
 			// Only allow $set if presenterId is known
-			&& modifier.$set.presenterId === doc.presenterId
+			modifier.$set.presenterId === doc.presenterId
 		);
 	}
 });
